b26bdb93752589d167ee8fefee9a93e9db6462d8
Ayman Shaikh <Ayman.Shaikh@arm.com>
2025-11-29 22:58:59 +0000

    Frontend Developer: Created Anam UI component.

---
+++ src/components/AnamUI.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AnamUI.css';
+
+const AnamUI = () => {
+  return (
+    <div className="anam-container">
+      <h1>Welcome to Anam UI!</h1>
+      <button className="anam-button">Click Me</button>
+    </div>
+  );
+};
+
+export default AnamUI;
